a2731849b05f6389f4b3cf139ae9bdeb91e88189
lnaresh <leelanaresh.b@gmail.com>
2022-08-07 19:59:01 +0530

    main_change_ftr1: added printL functionality

--- cproject/main.c
+++ cproject/main.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 int feature1(){
-printk("kernel mainchange_feature1 functionality");
+printl("kernel mainchange_feature1 functionality");
 
 }
 
 int main(){
-printf("1:Kernel Functionality Patch\n");
-printf("2:Functionality Patch\n");
+printl("1:Kernel Functionality Patch\n");
+printl("2:Functionality Patch\n");
 }
